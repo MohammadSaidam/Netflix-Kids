b8cb9531cb91e36946659438b60307503bce2718
MohammadSaidam <msaidam58@gmail.com>
2024-09-12 10:47:43 +0200

    Initial Commit

---
+++ Netflix Kids/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix Kids
+//
+//  Created by Mohammed Saidam on 12/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
